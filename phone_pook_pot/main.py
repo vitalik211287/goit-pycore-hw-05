2345e822182ad4e0c20e06e00368234a17f0c1ef
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2024-10-20 13:54:15 +0300

    done HW5

--- phone_pook_pot/main.py
+++ phone_pook_pot/main.py
@@ -1,28 +1,29 @@
-from parser import parse_input  
 from contacts import add_contact, change_contact, show_phone, show_all  
 
-def main():  
-    print("Welcome to the assistant bot!")  # Додаємо вітання  
     
-    while True:  
-        command_input = input("Enter a command: ")  # Отримуємо команду від користувача  
-        cmd, args = parse_input(command_input)  # Парсимо команду та аргументи  
-        
-        if cmd in ["close", "exit"]:  
-            print("Good bye!")  
-            break  
-        elif cmd == "hello":  
-            print("How can I help you?")  
-        elif cmd == "add":  
-            print(add_contact(args))  
-        elif cmd == "change":  
-            print(change_contact(args))  
-        elif cmd == "phone":  
-            print(show_phone(args))  
-        elif cmd == "all":  
-            print(show_all())  
-        else:  
-            print("Invalid command.")  
+def main():
+    print("Welcome to the assistant bot!")
+    while True:
+        command = input("Enter a command: ").lower()
+        match command:
+            case "close" | "exit":
+                print("Good bye!")
+                break
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                args = input("Enter name and phone: ").split()
+                print(add_contact(args))
+            case "change":
+                args = input("Enter name and new phone: ").split()
+                print(change_contact(args))
+            case "phone":
+                args = input("Enter name: ").split()
+                print(show_phone(args))
+            case "all":
+                print(show_all())
+            case _:
+                print("Invalid command.")
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     main()
